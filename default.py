25bec951e8010459d944099663e4e1a25d131a10
Anessia <anessia@teztechnology.com>
2024-02-15 16:57:24 -0600

    added defaults

--- default.py
+++ default.py
@@ -1,3 +1,20 @@
-#these are the standards or defaults 
-pagebackground = f7f8fa
+"""
+this file is dedicated to housing all default values
+"""
+
+#Layout & Styles section
+
+#Hexcolors
+navigationActive = "#C6B7FF"
+navigationActiveText = "#000000"
+navigationText =  "#000000"
+navigationBackground = "#FFFFFF"
+highlightField = "#000000"
+pageBackground = "#f6f8fa"
+headerBackground = "#FFFFFF"
+headerText = "#000000"
+
 maxwidth = 1280
+
+tabimage = "standardlogo.jpg"
+customerLogo = "tezlenz.png"
