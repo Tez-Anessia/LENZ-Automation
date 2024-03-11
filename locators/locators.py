89903cb5b9ac35ac7658ec593275ef76215e2242
Anessia <anessia@teztechnology.com>
2024-03-11 15:28:12 -0500

    updated most locators, 3 remaining

--- locators/locators.py
+++ locators/locators.py
@@ -127,5 +127,6 @@ class wsSettingsTab:
     filterOperatordropdown = (By.XPATH, "//div[@class='w-full h-full flex justify-between items-center']//button[contains(@class,'h-10 relative')]")
     equaltoOption = (By.XPATH, "(//li[@role='option'])[1]")
     allOptions = (By.XPATH, "(//li[@role='option'])")
+    textValueInput = (By.XPATH, "//input[@name='column-value']")
     
     deleteWorkspace = (By.XPATH, "//button[@class='group inline-flex items-center justify-center transition-all duration-200 rounded-md border px-4 py-2 text-regular font-medium focus:ring-2 focus:border-highlightColor bg-white text-highlightColor border-[1px] border-highlightColor hover:shadow-sm  sm:w-auto h-12 !text-red-300 !border-red-300']")
